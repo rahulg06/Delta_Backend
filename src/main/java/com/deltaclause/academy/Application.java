fee7918b26b0d9543fdea54795c000bdda8233ba
rahulg06 <rahulguptaendless@gmail.com>
2026-06-20 01:23:23 +0530

    Initial commit

--- src/main/java/com/deltaclause/academy/Application.java
+++ src/main/java/com/deltaclause/academy/Application.java
@@ -2,9 +2,25 @@ package com.deltaclause.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 
 @SpringBootApplication
 public class Application {
+
+
+    @PostConstruct
+    public void testGmailConnection() {
+        try (Socket socket = new Socket()) {
+            System.out.println("Testing smtp.gmail.com:587...");
+            socket.connect(new InetSocketAddress("smtp.gmail.com", 587), 10000);
+            System.out.println("SUCCESS: Gmail SMTP reachable");
+        } catch (Exception e) {
+            System.out.println("FAILED: Gmail SMTP NOT reachable");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
